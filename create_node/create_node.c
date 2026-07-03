f8debcbc8b1a934ab8cdee33b792e96a89fd2d8a
lukehuang880118 <luke26123720@gmail.com>
2026-07-03 10:30:22 +0800

    create_node update

---
+++ create_node/create_node.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+struct listnode
+{	
+	int data;
+	struct listnode* next;
+};
+typedef struct listnode node;
+
+//(node*類型的指標地址)建立新的鏈結陣列節點(節點資料)
+node* create_node(char data);
+
+node* create_node(char data)
+{
+    node* ptr = (node*)malloc(sizeof(node));
+    if (ptr == NULL)
+    {
+        printf("create_node::malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    ptr->next = NULL;
+    ptr->data = data;
+    return ptr;
+}
