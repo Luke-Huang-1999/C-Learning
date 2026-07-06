a18f7d00424ce2d1884c3cc3a79c9b7a7c2fdf3c
lukehuang880118 <luke26123720@gmail.com>
2026-07-06 15:50:41 +0800

    modify BSF() function

--- Adjacency Matrix and List/Adjacency Matrix and List.c
+++ Adjacency Matrix and List/Adjacency Matrix and List.c
@@ -30,15 +30,33 @@ void print_adjmatrix(int matrix[row][col]);
 //()列印鄰接陣列(一維的node*陣列)
 void print_adjlist(node* adj_list[row]);
 
+void BSF(char data, node* adj_list[row]);
+
+node* search_adjlist(char data, node* adj_list[row]);
+
+node* enqueue(node* ptr, node* current);
+
+node* dequeue(node* front);
+
+void print_single_adjlist(node* ptr);
+
 int main()
 {
     FILE* fptr = fopen("adj_list.txt", "r");
-    check_fopen(fptr);
+
+
     int matrix[row][col] = { 0 };
     node* adj_list[row] = { NULL };
+    
     create_adjmatrix(fptr, matrix);
     create_adjlist(matrix, adj_list);
+    print_adjmatrix(matrix);
+    printf("=====================================================\n");
     print_adjlist(adj_list);
+    printf("=====================================================\n");
+
+    
+    BSF('A', adj_list);
 
 
     fclose(fptr);
@@ -134,5 +152,77 @@ void print_adjlist(node* adj_list[row])
             current = current->next;
         }
         printf("\n");
+    }
+}
+
+void BSF(char data, node* adj_list[row])
+{
+    char queue[row] = { 0 };    //記錄一維陣列資料
+    node* current = NULL;       //指向鄰接陣列adj_list[row]
+    int front = 0;         //queue鏈結陣列前端
+    int rear = 0;          //queue鏈結陣列後端
+    int visited[row] = { 0 };   //確認是否被拜訪過；為1則不加入queue，0反之
+    int num = 0;
+    current = search_adjlist(data, adj_list);   //指向第一個走訪的資料地址
+    int i = 0;
+
+    while (i < row)
+    {
+        while (current != NULL)//入隊
+        {
+            num = (current->data) - 'A';
+            if (visited[num] == 0)//未被拜訪
+            {
+                queue[i] = current->data;
+                rear = i;
+                i = i + 1;
+            }
+            visited[num] = 1;
+            current = current->next;
+        }
+        
+        //出隊
+        printf("%c ", queue[front]);
+        front = front + 1;
+
+        current = search_adjlist(queue[front], adj_list);
+    }
+
+}
+
+node* enqueue(node* ptr, node* current)//入隊
+{
+    node* newnode = create_node(current->data);
+    ptr->next = newnode;
+    ptr = newnode;
+    return ptr;
+}
+
+node* dequeue(node* front)//出隊
+{
+    printf("%c ", front->data);
+    front = front->next;
+    return front;
+}
+
+node* search_adjlist(char data, node* adj_list[row])
+{
+    int i;
+    for (i = 0; i < row; i++)
+    {
+        if (adj_list[i]->data == data)
+            return adj_list[i];
+    }
+
+    printf("search_adjlist::char not exist\n");
+    return NULL;
+}
+
+void print_single_adjlist(node* ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%c ", ptr->data);
+        ptr = ptr->next;
     }
 }
