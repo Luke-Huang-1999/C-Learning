576aa8aa9629f91733e965b610dfa7789f4c1fd5
lukehuang880118 <luke26123720@gmail.com>
2026-07-12 22:25:23 +0800

    Adding  Enqueue and Dequeue function

--- Queue(Push and Pop)/Queue(Push and Pop).c
+++ Queue(Push and Pop)/Queue(Push and Pop).c
@@ -10,13 +10,18 @@ struct queue
 typedef struct queue node;
 
 node* create_node(char data);
-node* create_queue(char str[], node* ptr);
+node* create_queue(char str[], node* front);
 void print_queue(node* ptr);
+node* enqueue(char data, node* front);
+node* dequeue(char data, node* front);
 int main()
 {
 	char str[] = { 'A','B' ,'C' ,'1' ,'2' ,'3' ,'4' };
 	node* queue = NULL;
 	queue = create_queue(str, queue);
+	queue = enqueue('5', queue);
+	queue = dequeue('5', queue);
+
 	print_queue(queue);
 	return 0;
 }
@@ -35,17 +40,23 @@ node* create_node(char data)
 	return newnode;
 }
 
-node* create_queue(char str[], node* ptr)
+node* create_queue(char str[], node* front)
 {
 	int i;
+	front = NULL;
+	node* rear = NULL;
 	for (i = 0 ; i < size; i++)
 	{
 		node* newnode = create_node(str[i]);
-		if (ptr != NULL)
-			newnode->next = ptr;
-		ptr = newnode;
+		if (front == NULL)
+			front = newnode;
+		else
+			rear->next = newnode;
+
+		rear = newnode;
+
 	}
-	return ptr;
+	return front;
 }
 
 void print_queue(node* ptr)
@@ -63,5 +74,59 @@ void print_queue(node* ptr)
 }
 
 
+node* enqueue(char data, node* front)
+{
+	node* rear = front;
+	while (rear->next != NULL)
+		rear = rear->next;
 
+	node* newnode = create_node(data);
+	rear->next = newnode;
+	
 
+	return front;
+}
+
+node* dequeue(char data, node* front)
+{
+	node* rear = front;
+	node* previous = NULL;
+	node* tmp = NULL;
+	while (rear != NULL)
+	{
+		if (rear->data == data)
+			break;
+
+		previous = rear;
+		rear = rear->next;
+	}
+	if (rear == NULL)
+	{
+		printf("Can not find the char.\n");
+		exit(1);
+	}
+
+	if (rear->next == NULL)//末節點刪除
+	{
+		tmp = rear;
+		rear = previous;
+		rear->next = NULL;
+		free(tmp);
+	}
+	else if (previous == NULL)//首節點刪除
+	{
+		tmp = rear;
+		front = rear->next;
+		rear = rear->next;
+		free(tmp);
+	}
+	else
+	{
+		tmp = rear;
+		rear = rear->next;
+		previous->next = rear;
+		free(tmp);
+	}
+
+	return front;
+}
