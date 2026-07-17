935bd72260daf1eda7e09d230219ddc3bac644ab
lukehuang880118 <luke26123720@gmail.com>
2026-07-17 13:48:14 +0800

    short path update

--- TEST/test.c
+++ TEST/test.c
@@ -1,39 +1,35 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#define row 7
-#define col 7
-struct node
+#define row 6
+#define col 6
+
+typedef struct node
 {
-	int data;
+	char p1;
+	char p2;
+	int weight;
 	struct node* next;
-};
-typedef struct node node;
+}node;
 
 FILE* file_open(char file_name[]);
-void create_adjmatrix(FILE* fptr, int adj_matrix[row][col]);
-void create_adjlist(int adj_matrix[row][col], node* adjlist[]);
-node* create_node(int data);
-void print_adjlist(node* adjlist[]);
-void print_adjmatrix(int adj_matrix[row][col]);
-void BFS(int start, node* adj_list[]);
-void DFS(int start, node* adj_list[]);
-
+node* create_node(char p1, char p2, int weight);
+void create_adjmatrix(FILE* fptr, int adjmatrix[row][col]);
+void print_adjmatrix(int adjmatrix[row][col]);
+node* create_adjlist(node* adjlist[row], int adjmatrix[row][col]);
+void print_adjlist(node* adjlist[row]);
 int main()
 {
-	FILE* fptr = file_open("adj_list.txt");
-	int adj_matrix[row][col] = { 0 };
-	node* adj_list[row] = { NULL };
-	create_adjmatrix(fptr, adj_matrix);
-	create_adjlist(adj_matrix, adj_list);
-	printf("adjmatrix\n---------------------------------------\n");
-	print_adjmatrix(adj_matrix);
-	printf("adjlist\n---------------------------------------\n");
-	print_adjlist(adj_list);
-	printf("BFS\n---------------------------------------\n");
-	BFS(5,adj_list);
-	printf("DFS\n---------------------------------------\n");
-	DFS(1,adj_list);
+	FILE* fptr = file_open("adjmatrix.txt");
+	int adjmatrix[row][col] = { 0 };
+	node* adjlist[row] = { NULL };
+	create_adjmatrix(fptr, adjmatrix);
+	printf("adjmatrix print:\n");
+	print_adjmatrix(adjmatrix);
+
+	create_adjlist(adjlist, adjmatrix);
+	print_adjlist(adjlist);
+
 
 	fclose(fptr);
 	return 0;
@@ -42,66 +38,88 @@ int main()
 FILE* file_open(char file_name[])
 {
 	FILE* fptr = fopen(file_name, "r");
-
 	if (fptr == NULL)
 	{
-		printf("Failed to open the file.\n");
+		printf("Failed to open file.\n");
 		exit(1);
 	}
+
 	return fptr;
 }
 
-void create_adjmatrix(FILE* fptr, int adj_matrix[row][col])
-{
-	int i, j;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-			fscanf(fptr, "%d", &adj_matrix[i][j]);
-	}
-}
-
-void create_adjlist(int adj_martrix[row][col], node* adjlist[])
-{
-	node* current = NULL;
-	node* newnode = NULL;
-	int i, j;
-
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			if (adj_martrix[i][j] == 1)
-			{
-				newnode = create_node(j + 1);
-				if (current == NULL)
-					adjlist[i] = newnode;
-				else
-					current->next = newnode;
-
-				current = newnode;
-			}
-		}
-		current = NULL;
-	}
-	
-}
-
-node* create_node(int data)
+node* create_node(char p1, char p2, int weight)
 {
 	node* newnode = (node*)malloc(sizeof(node));
 	if (newnode == NULL)
 	{
-		printf("Failed to create newnode.\n");
+		printf("create_node function error.\n");
+		printf("newnode is NULL\n");
 		exit(1);
 	}
-	newnode->data = data;
+
+	newnode->p1 = p1;
+	newnode->p2 = p2;
+	newnode->weight = weight;
 	newnode->next = NULL;
 
 	return newnode;
 }
 
-void print_adjlist(node* adjlist[])
+void create_adjmatrix(FILE* fptr, int adjmatrix[row][col])
+{
+	int i, j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			fscanf(fptr, "%d", &adjmatrix[i][j]);
+		}
+	}
+}
+
+void print_adjmatrix(int adjmatrix[row][col])
+{
+	int i, j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			printf(" %d ", adjmatrix[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+node* create_adjlist(node* adjlist[row], int adjmatrix[row][col])
+{
+	int i, j;
+	node* current = NULL;
+	for (i = 0; i < row; i++)
+	{
+		current = adjlist[i];
+		for (j = 0; j < col; j++)
+		{
+			if (adjmatrix[i][j] != 0)
+			{
+				node* newnode = create_node(('A' + i), ('A' + j), adjmatrix[i][j]);
+				if (current == NULL)
+				{
+					current = newnode;
+					adjlist[i] = newnode;
+				}
+				else
+				{
+					current->next = newnode;
+					current = newnode;
+				}
+			}
+		}
+	}
+	return adjlist[0];
+}
+
+void print_adjlist(node* adjlist[row])
 {
 	int i;
 	node* current = NULL;
@@ -110,103 +128,13 @@ void print_adjlist(node* adjlist[])
 		current = adjlist[i];
 		while (current != NULL)
 		{
-			printf("%d ", current->data);
+			//printf("p1 = %c, p2 = %c, weight = %d, address = %p, next address = %p\n", current->p1, current->p2, current->weight, current,current->next);
+			printf("p1 = %c, p2 = %c, weight = %d\n", current->p1, current->p2, current->weight);
 			current = current->next;
 		}
 		printf("\n");
 	}
-	printf("\n");
+
 }
 
-void print_adjmatrix(int adj_matrix[row][col])
-{
-	int i, j;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < col; j++)
-		{
-			printf("%d ", adj_matrix[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
 
-void BFS(int start, node* adj_list[])//廣度探索_佇立
-{
-	//佇立用
-	node* front = NULL;
-	node* rear = NULL;
-	//鄰接串列
-	node* current = NULL;
-	//是否被拜訪
-	int visited[row] = { 0 };
-	//第一個點進入佇立
-	front = create_node(start);
-	rear = front;
-	visited[front->data - 1] = 1;
-
-	while (front != NULL)
-	{
-		//pop
-		int num = front->data;
-		printf("%d ", front->data);
-		
-		//push
-		current = adj_list[num - 1];
-		while (current != NULL)
-		{
-			if (visited[current->data-1] == 0)
-			{
-				node* newnode = create_node(current->data);
-				rear->next = newnode;
-				rear = newnode;
-
-				visited[current->data-1] = 1;
-			}
-			current = current->next;
-		}
-		node* tmp = front;
-		front = front->next;
-		free(tmp);
-		tmp = NULL;
-	}
-	rear = NULL;
-	printf("\n\n");
-}
-
-void DFS(int start, node* adj_list[])//堆疊
-{
-	//堆疊用
-	node* top = NULL;
-	//鄰接串列用
-	node* current = NULL;
-	int visited[row] = { 0 };
-	
-	//首點建立
-	top = create_node(start);
-	visited[start - 1] = 1;
-
-	while (top != NULL)
-	{
-		current = adj_list[top->data - 1];
-		//pop
-		printf("%d ", top->data);
-		top = top->next;
-		
-		//push
-		while (current != NULL)
-		{
-			if (visited[current->data - 1] == 0)
-			{
-				node* newnode = create_node(current->data);
-				newnode->next = top;
-				top = newnode;
-
-				visited[current->data - 1] = 1;
-			}
-			current = current->next;
-		}
-		
-	}
-}
