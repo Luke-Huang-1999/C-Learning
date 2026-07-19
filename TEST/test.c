3d5fe82202a50e811f09d34a2008135adbb807b9
lukehuang880118 <luke26123720@gmail.com>
2026-07-19 14:54:33 +0800

    Prim

--- TEST/test.c
+++ TEST/test.c
@@ -18,10 +18,8 @@ void create_adjmatrix(FILE* fptr, int adjmatrix[row][col]);
 void print_adjmatrix(int adjmatrix[row][col]);
 node* create_adjlist(node* adjlist[row], int adjmatrix[row][col]);
 void print_adjlist(node* adjlist[row]);
-node* sort(node* adjlist[row]);
-void print_single_list(node* sortlist);
-void add_parent(char from, char to);
-void low_cost(node* adjlist);
+
+
 
 int main()
 {
@@ -43,15 +41,6 @@ int main()
 	printf("adjlist print:\n");
 	print_adjlist(adjlist);//鄰接鏈結串列
 	printf("================================\n\n");
-	
-	//排序
-	node* sortlist = sort(adjlist);
-	printf("sortlist print:\n");
-	print_single_list(sortlist);//列印排序
-	printf("================================\n");
-
-	//祖先設定
-	add_parent('C', 'A');
 	
 
 	//關閉檔案
@@ -161,97 +150,4 @@ void print_adjlist(node* adjlist[row])
 
 }
 
-node* sort(node* adjlist[row])
-{
-	int i, j;
-	int line_cnt = 0;
-	node* current = NULL;
-	node* sortlist = NULL;
-	node* sortlist_head = NULL;
-	for (i = 0; i < row; i++)
-	{
-		current = adjlist[i];
-		while (current != NULL)
-		{
-			node* newnode = create_node(current->p1, current->p2, current->weight);
-			if (sortlist == NULL)
-			{
-				sortlist_head = newnode;
-				sortlist = newnode;
-			}
-			else
-			{
-				sortlist->next = newnode;
-				sortlist = newnode;
-			}
 
-			line_cnt++;
-			current = current->next;
-		}
-	}
-
-	for (i = 0; i < (line_cnt - 1); i++)
-	{
-		sortlist = sortlist_head;
-		while (sortlist->next != NULL)
-		{
-			if (sortlist->weight > sortlist->next->weight)
-			{
-				char tmp_p1 = sortlist->p1;
-				char tmp_p2 = sortlist->p2;
-				int tmp_weight = sortlist->weight;
-
-				sortlist->p1 = sortlist->next->p1;
-				sortlist->p2 = sortlist->next->p2;
-				sortlist->weight = sortlist->next->weight;
-
-				sortlist->next->p1 = tmp_p1;
-				sortlist->next->p2 = tmp_p2;
-				sortlist->next->weight = tmp_weight;
-			}
-			sortlist = sortlist->next;
-		}
-	}
-
-	//node* test = sortlist_head;
-	//while (test != NULL)
-	//{
-	//	printf("p1 = %c, p2 = %c, weight = %d\n", test->p1, test->p2, test->weight);
-	//	test = test->next;
-	//}
-
-	return sortlist_head;
-}
-
-void print_single_list(node* sortlist)
-{
-	while (sortlist != NULL)
-	{
-		printf("p1 = %c, p2 = %c, weight = %d\n", sortlist->p1, sortlist->p2, sortlist->weight);
-		sortlist = sortlist->next;
-	}
-}
-
-void add_parent(char from, char to)
-{
-	char parent[row] = { 'A','B','C' ,'D' ,'E' ,'F' };
-	int i;
-	for (i = 0; i < row; i++)
-	{
-		if (parent[i] == from)
-			parent[i] = to;
-	}
-
-
-	printf("parent[row] = ");
-	for (i = 0; i < row; i++)
-	{
-		printf("%c ", parent[i]);
-	}
-
-}
-
-void low_cost(node* adjlist)
-{
-	
-}
