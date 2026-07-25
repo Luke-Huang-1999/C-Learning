6211cb61e6573650858efcc35a92dcccdb3453c8
lukehuang880118 <luke26123720@gmail.com>
2026-07-25 15:47:45 +0800

    pratice

--- TEST/test.c
+++ TEST/test.c
@@ -1,2 +1,30 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
 
+void change(int str_1[], int str_2[]);
+
+int main()
+{
+	int str_1[3] = { 1,2,3 };
+	int str_2[3] = { 0 };
+
+	change(str_1, str_2);
+
+
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		printf("%d ", str_2[i]);
+	}
+	return 0;
+}
+
+void change(int str_1[], int str_2[])
+{
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		str_2[i] = str_1[i];
+	}
+	
+}
