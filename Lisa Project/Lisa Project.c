0ef50bfd0e20e57102b835238964217f39ed75ba
lukehuang880118 <luke26123720@gmail.com>
2026-08-01 15:23:15 +0800

    Lisa's project

--- Lisa Project/Lisa Project.c
+++ Lisa Project/Lisa Project.c
@@ -1,57 +1,73 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#define row 6
-#define col 7
+
+#define CAL_ROWS 6			//6行
+#define CAL_COLS 7			//一週7天
+#define EMPLOYEE_NUMS 6		//員工人數
+#define WORK_DAY_MAX 3		//最多連續做幾天
+#define EMPTY_DAY 0			//月曆中空白日的賦值
+#define SHIFT_NUMS 3		//班別(早班午班晚班)
+#define NAME_LEN 20			//員工名字長度限制20bytes
+
 typedef struct node
 {
-	int day;
-	char employee_1[10];
-	char employee_2[10];
+	int date;
+	char employee[SHIFT_NUMS][NAME_LEN];
+	int work_day[SHIFT_NUMS];
+	//int work_day_2;
 }node;
 
-char employee_all[4][15] = {"AAAAA","BBBBB","CCCCC" ,"DDDDD"};
+typedef struct employinfo
+{
+	char employee[10];
+	int day;
+}epinfo;
 
-void recieve_data(int* year, int* month);
+char employee_all[EMPLOYEE_NUMS][NAME_LEN] = {"Alice","Bruce","Carol" ,"David","Eason","Frank"};
+
+void receive_data(int* year, int* month);
 //(回傳當年當月的1號星期幾)蔡勒公式(年份, 月份)
 
 int Zeller(int year, int month);
 //()建立月曆(當年當月的1號星期幾)
-void calendar(int year, int month, node cale[row][col]);
+void create_calendar(int year, int month, node cale[CAL_ROWS][CAL_COLS]);
 
 //(回傳當月天數)計算當月天數(年份, 月份)
 int month_days(int year, int month);
 
-void print_calendar(node cale[row][col]);
-// A/B/C三人，做三休一
+//()列印月曆(node型態的二維矩陣)
+void print_calendar_console(node cale[CAL_ROWS][CAL_COLS]);
+
+
 
 int main()
 {
 	//宣告變數
 	int year, month;
-	node cale[row][col] = { 0 };
+	node cale[CAL_ROWS][CAL_COLS] = { 0 };
 	year = 2026;
 	month = 8;
 
 	//1.接收初始資訊
-	//recieve_data(&year, &month);	//年月
+	//receive_data(&year, &month);	//年月
 
 	//生成月曆
-	calendar(year, month, cale);
+	create_calendar(year, month, cale);
 
 	//列印月曆
-	print_calendar(cale);
+	print_calendar_console(cale);
 
 
+	//system("pause");
 
 	return 0;
 }
 
-void recieve_data(int* year, int* month)
+void receive_data(int* year, int* month)
 {
 	printf("請輸入年份與月份：");
 	scanf("%d %d", year, month);
-
 }
 
 int Zeller(int year, int month)
@@ -82,62 +98,81 @@ int Zeller(int year, int month)
 	return week;
 }
 
-void calendar(int year, int month, node cale[row][col])
+void create_calendar(int year, int month, node cale[CAL_ROWS][CAL_COLS])
 {
 	int week = Zeller(year, month); //6
-	//星期標題建立
-	char all_week[7][4] = { "Mon","Tue","Wed" ,"Thu" ,"Fri" ,"Sat" ,"Sun" };
 	int i, j;
-	for (i = 0; i < 7; i++)
-	{
-		printf("%15s", all_week[i]);
-	}
-	printf("\n");
 
 	int day_cnt = 1;
 	int week_cnt = 0;
-	int max_day = month_days(year, month);
+	int day_max = month_days(year, month);
 	int blank_day = week - 1; //5
-	int empolyee_source_1 = 0;
-	int empolyee_source_2 = 0;
+	int no_1 = 0;
+	int no_2 = 1;
+	int wd_1 = 0;
+	int wd_2 = 1;
 
-	while (day_cnt < max_day)
+	for (i = 0; i < CAL_ROWS; i++)
 	{
-		for (i = 0; i < row; i++)
+		for (j = 0; j < CAL_COLS; j++)
 		{
-			for (j = 0; j < col; j++)
+			if (blank_day > 0)							//前面空格
 			{
-				if (blank_day > 0)							//前面空格
+				cale[i][j].date = EMPTY_DAY;
+				strcpy(cale[i][j].employee[0], "###");
+				strcpy(cale[i][j].employee[1], "###");
+				cale[i][j].work_day[0] = 0;
+				cale[i][j].work_day[1] = 0;
+				blank_day--;
+			}
+			else if (day_cnt <= day_max)				//中間班表
+			{
+				if (wd_1 < WORK_DAY_MAX)
 				{
-					cale[i][j].day = 32;
-					strcpy(cale[i][j].employee_1, "###");
-					strcpy(cale[i][j].employee_2, "###");
-					blank_day--;
+					wd_1++;
+					cale[i][j].work_day[0] = wd_1;
 				}
-				else if (day_cnt <= max_day)				//中間班表
+				else
 				{
-					empolyee_source_1 = day_cnt / 4;
-					empolyee_source_2 = day_cnt / 4;
+					wd_1 = 1;
+					no_1 = (no_1 + 2) % 4;
+					cale[i][j].work_day[0] = wd_1;
+				}
 
-					cale[i][j].day = day_cnt;
-					strcpy(cale[i][j].employee_1, employee_all[empolyee_source_1]);
-					strcpy(cale[i][j].employee_2, "BBBBB");
-					day_cnt++;
-				}
-				else										//後面空格
+				if (wd_2 < WORK_DAY_MAX)
 				{
-					cale[i][j].day = 32;
-					strcpy(cale[i][j].employee_1, "##");
-					strcpy(cale[i][j].employee_2, "##");
+					wd_2++;
+					cale[i][j].work_day[1] = wd_2;
 				}
+				else
+				{
+					wd_2 = 1;
+					no_2 = (no_2 + 2) % 4;
+					cale[i][j].work_day[1] = wd_2;
+				}
+
+				cale[i][j].date = day_cnt;
+				strcpy(cale[i][j].employee[0], employee_all[no_1]);
+				strcpy(cale[i][j].employee[1], employee_all[no_2]);
+
+				day_cnt++;
+			}
+			else										//後面空格
+			{
+				cale[i][j].date = EMPTY_DAY;
+				strcpy(cale[i][j].employee[0], "##");
+				strcpy(cale[i][j].employee[1], "##");
+				cale[i][j].work_day[0] = 0;
+				cale[i][j].work_day[1] = 0;
 			}
 		}
 	}
+
 }
 
 int month_days(int year, int month)
 {
-	int days;
+	int days = 0;
 
 	switch (month)
 	{
@@ -164,42 +199,58 @@ int month_days(int year, int month)
 		else
 			days = 28;
 		break;
+
+	default:
+		return 0;
 	}
 
 	return days;
 }
 
-void print_calendar(node cale[row][col])
+void print_calendar_console(node cale[CAL_ROWS][CAL_COLS])
 {
+	//星期標題建立
+	char all_week[7][4] = { "Mon","Tue","Wed" ,"Thu" ,"Fri" ,"Sat" ,"Sun" };
 	int i, j;
-	for (i = 0; i < row; i++)
+	for (i = 0; i < 7; i++)
+	{
+		printf("%15s", all_week[i]);
+	}
+	printf("\n");
+	for (i = 0; i < (15 * 7); i++)
+		printf("=");
+	printf("\n");
+
+	for (i = 0; i < CAL_ROWS; i++)
 	{
 		//列印日期
-		for (j = 0; j < col; j++)
+		for (j = 0; j < CAL_COLS; j++)
 		{
-			if (cale[i][j].day != 32)
-				printf("%15d", cale[i][j].day);
+			if (cale[i][j].date != EMPTY_DAY)
+				printf("%15d", cale[i][j].date);
 			else
 				printf("%15s", " ");
 		}
 		printf("\n");
 		//列印員工1
-		for (j = 0; j < col; j++)
+		for (j = 0; j < CAL_COLS; j++)
 		{
-			if (cale[i][j].day != 32)
-				printf("%15s", cale[i][j].employee_1);
+			if (cale[i][j].date != EMPTY_DAY)
+				printf("%15s", cale[i][j].employee[0]);
 			else
 				printf("%15s", " ");
 		}
 		printf("\n");
 		//列印員工2
-		for (j = 0; j < col; j++)
+		for (j = 0; j < CAL_COLS; j++)
 		{
-			if (cale[i][j].day != 32)
-				printf("%15s", cale[i][j].employee_2);
+			if (cale[i][j].date != EMPTY_DAY)
+				printf("%15s", cale[i][j].employee[1]);
 			else
 				printf("%15s", " ");
 		}
-		printf("\n");
+		printf("\n\n");
+		
 	}
 }
+
