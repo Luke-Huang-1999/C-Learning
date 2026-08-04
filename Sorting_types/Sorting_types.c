e00c74f963a87c280f55569330cef852762be6c0
lukehuang880118 <luke26123720@gmail.com>
2026-08-04 21:57:07 +0800

    update merge function

--- Sorting_types/Sorting_types.c
+++ Sorting_types/Sorting_types.c
@@ -13,7 +13,8 @@ void selection_sort(int* arr);
 void insertion_sort(int* arr);
 
 //合併排序法(一維陣列首元素地址)
-void merge_sort(int* arr, int left, int right);
+void merge_sort(int arr[], int left, int right);
+void merge(int arr[], int left, int mid, int right);
 
 //()列印一維陣列(一維陣列首元素地址)
 void print_arr(int* arr);
@@ -56,7 +57,7 @@ int main()
 	//printf("    Merge Sort：");
 	//print_arr(arr);
 
-	//希爾排序
+	//合併排序
 	merge_sort(arr,0,nums - 1);
 	printf("    Merge Sort：");
 	print_arr(arr);
@@ -133,10 +134,44 @@ void insertion_sort(int* arr)
 	}
 }
 
-void merge_sort(int* arr, int left, int right)
+void merge_sort(int arr[], int left, int right)
 {
-	
+	if (left >= right)
+		return;
 
+	int mid = (left + right) / 2;
+
+	merge_sort(arr, left, mid);
+
+	merge_sort(arr, mid + 1, right);
+
+	merge(arr, left, mid, right);
+}
+
+void merge(int arr[], int left, int mid, int right)
+{
+	int temp[nums];
+
+	int i = left;
+	int j = mid + 1;
+	int k = left;
+
+	while (i <= mid && j <= right)
+	{
+		if (arr[i] <= arr[j])
+			temp[k++] = arr[i++];
+		else
+			temp[k++] = arr[j++];
+	}
+
+	while (i <= mid)
+		temp[k++] = arr[i++];
+
+	while (j <= right)
+		temp[k++] = arr[j++];
+
+	for (i = left; i <= right; i++)
+		arr[i] = temp[i];
 }
 
 void print_arr(int* arr)
--- TEST/test.c
+++ TEST/test.c
@@ -2,17 +2,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int test(int i);
+
 int main()
 {
 
-	int a = 1;
-	int b = 0;
-	if (a % 2 != 0)
-	{
-		b = 1;
-	}
-	else
-		b = 2;
+	int i = 10;
+	test(10);
 
 	return 0;
+}
+
+int test(int n)
+{
+	if (n == 0)      // 終止條件
+		return 0;
+
+	return n + test(n - 1);
 }
